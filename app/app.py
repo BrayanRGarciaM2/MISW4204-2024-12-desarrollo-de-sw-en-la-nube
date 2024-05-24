b11f27e159e75468d0b62711bcf8ce876b9aa75f
j.garcia55 <j.garcia55@uniandes.edu.co>
2024-05-23 23:22:16 -0500

    actualziacion cloud sql

--- app/app.py
+++ app/app.py
@@ -12,7 +12,7 @@ from vistas.VistaTarea import VistaTarea
 
 def create_flask_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@/videos?unix_socket=/cloudsql/idrl-videos-202410:us-central1:db-videos'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost:5432/videos?unix_socket=/cloudsql/idrl-videos-202410:us-central1:db-videos'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = 'frase-secreta'
     app.config['PROPAGATE_EXCEPTIONS'] = True
